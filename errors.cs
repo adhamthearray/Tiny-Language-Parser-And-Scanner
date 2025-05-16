ef75061fce0590c53b6a94f758eb88376b1f7aff
adhamthearray <dommawalid@gmail.com>
2025-05-16 22:07:55 +0300

    Create errors.cs

---
+++ errors.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JASON_Compiler
+{
+    public static class Errors
+    {
+       public static List<string> Error_List = new List<string>();
+    }
+}
